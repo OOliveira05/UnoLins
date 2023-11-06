16a7e8c6b04997ea20ff8f0470487de11e709e77
Paulo Henrique <paulo_phs01@hotmail.com>
2023-11-06 20:49:14 -0300

    telas cadastro SA e consulta ensaio

--- components/CadastroSolicitacaoAnaliseScreen.tsx
+++ components/CadastroSolicitacaoAnaliseScreen.tsx
@@ -1,6 +1,166 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import axios from 'axios';
 
 const CadastroSolicitacaoAnaliseScreen = () => {
-  
+  const [nomeProjeto, setNomeProjeto] = useState('');
+  const [prazoAcordado, setPrazoAcordado] = useState('');
+  const [tipoDeAnalise, setTipoDeAnalise] = useState('');
+  const [descricaoDosServicos, setDescricaoDosServicos] = useState('');
+  const [informacoesAdicionais, setInformacoesAdicionais] = useState('');
+  const [modoEnvioResultado, setModoEnvioResultado] = useState('');
+  const [solicitantes, setSolicitantes] = useState([]);
+  const [solicitanteSelecionado, setSolicitanteSelecionado] = useState(null);
+  const [responsavelAbertura, setResponsavelAbertura] = useState('');
 
-}
+  useEffect(() => {
+    axios.get('https://uno-lims.up.railway.app/solicitantes')
+      .then(response => {
+        setSolicitantes(response.data);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar lista de solicitantes:', error);
+      });
+  }, []);
+
+  const handleSolicitanteSelect = (solicitante) => {
+    setSolicitanteSelecionado(solicitante);
+  };
+
+  const cadastrarSolicitacaoAnalise = async () => {
+    try {
+      const response = await axios.post('https://uno-lims.up.railway.app/solicitacoes-de-analise', {
+        nomeProjeto,
+        prazoAcordado,
+        tipoDeAnalise,
+        descricaoDosServicos,
+        informacoesAdicionais,
+        modoEnvioResultado,
+        solicitante: solicitanteSelecionado ? solicitanteSelecionado.cnpj : null,
+        responsavelAbertura,
+      });
+
+      console.log('Solicitação de análise cadastrada com sucesso!', response.data);
+      alert('Solicitação de análise cadastrada com sucesso!');
+      setNomeProjeto('');
+      setPrazoAcordado('');
+      setTipoDeAnalise('');
+      setDescricaoDosServicos('');
+      setInformacoesAdicionais('');
+      setModoEnvioResultado('');
+      setSolicitanteSelecionado(null);
+      setResponsavelAbertura('');
+    } catch (error) {
+      console.error('Erro ao cadastrar solicitação de análise:', error);
+      alert('Erro ao cadastrar solicitação de análise!');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Cadastro de Solicitação de Análise</Text>
+      <TextInput
+        placeholder="Nome do Projeto"
+        value={nomeProjeto}
+        onChangeText={setNomeProjeto}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Prazo Acordado"
+        value={prazoAcordado}
+        onChangeText={setPrazoAcordado}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Tipo de Análise"
+        value={tipoDeAnalise}
+        onChangeText={setTipoDeAnalise}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Descrição dos Serviços"
+        value={descricaoDosServicos}
+        onChangeText={setDescricaoDosServicos}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Informações Adicionais"
+        value={informacoesAdicionais}
+        onChangeText={setInformacoesAdicionais}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Modo de Envio do Resultado"
+        value={modoEnvioResultado}
+        onChangeText={setModoEnvioResultado}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="Responsável Abertura"
+        value={responsavelAbertura}
+        onChangeText={setResponsavelAbertura}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Selecione um Solicitante:</Text>
+      <View style={styles.solicitantesContainer}>
+        {solicitantes.map(solicitante => (
+          <TouchableOpacity
+            key={solicitante.cnpj}
+            style={[
+              styles.solicitanteButton,
+              solicitante === solicitanteSelecionado && styles.selectedSolicitanteButton,
+            ]}
+            onPress={() => handleSolicitanteSelect(solicitante)}
+          >
+            <Text>{solicitante.nome}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      
+      <Button title="Cadastrar" onPress={cadastrarSolicitacaoAnalise} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  headerText: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  solicitantesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  solicitanteButton: {
+    margin: 8,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+  },
+  selectedSolicitanteButton: {
+    backgroundColor: '#e0e0e0',
+  },
+});
+
 export default CadastroSolicitacaoAnaliseScreen;
--- components/ConsultaEnsaioScreen.tsx
+++ components/ConsultaEnsaioScreen.tsx
@@ -1,9 +1,106 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator, ScrollView } from 'react-native';
+import axios from 'axios';
 
+const EnsaioInfo = ({ label, value }) => (
+  <View>
+    <Text style={styles.label}>{label}:</Text>
+    <Text style={styles.text}>{value}</Text>
+  </View>
+);
+
+const ErrorMessage = () => (
+  <View style={styles.errorContainer}>
+    <Text style={styles.errorText}>Ocorreu um erro ao carregar os dados.</Text>
+  </View>
+);
 
 const ConsultaEnsaioScreen = () => {
- 
+  const [loading, setLoading] = useState(true);
+  const [ensaios, setEnsaios] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://uno-lims.up.railway.app/ensaios')
+      .then(response => {
+        setEnsaios(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return <ErrorMessage />;
+  }
+
+  if (ensaios.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text>Não há ensaios disponíveis.</Text>
+      </View>
+    );
+  }
+
+  return (
+    <ScrollView style={styles.container}>
+      {ensaios.map(ensaio => (
+        <View key={ensaio.id}>
+          <EnsaioInfo label="Nome do Ensaio" value={ensaio.nomeEnsaio} />
+          <EnsaioInfo label="Especificação" value={ensaio.especificacao} />
+          <EnsaioInfo label="ID do Item de Análise" value={ensaio.itemDeAnaliseId} />
+          <View style={styles.separator} />
+        </View>
+      ))}
+    </ScrollView>
+  );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginBottom: 4,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 12,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#ccc',
+    marginBottom: 20,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorText: {
+    fontSize: 16,
+    color: 'red',
+  },
+});
 
 export default ConsultaEnsaioScreen;
